f01a588f5294bb1611e54f5d93cf48b3cdd019be
Darlen Kitz Suizo <suizoda@oocl.com>
2021-08-05 20:33:26 +0800

    [MarsRover][Kitz]TEST: Added should_return_1_0_E_when_execute_command_given_0_0_E_and_M() and should_return_0_0_W_when_execute_command_given_0_0_S_and_R()

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -81,4 +81,34 @@ class DemoTest {
 
     }
 
+    @Test
+    void should_return_0_0_W_when_execute_command_given_0_0_S_and_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0 , 0 , Direction.S);
+        RoverStatus expectedStatus = new RoverStatus(0 , 0, Direction.W);
+
+        //when
+        marsRover.executeCommand("R");
+
+        //then
+
+        assertEquals(expectedStatus.getDirection(),marsRover.getDirection());
+
+    }
+
+    @Test
+    void should_return_1_0_E_when_execute_command_given_0_0_E_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0 , 0 , Direction.E);
+        RoverStatus expectedStatus = new RoverStatus(1 , 0, Direction.E);
+
+        //when
+        marsRover.executeCommand("M");
+
+        //then
+
+        assertEquals(expectedStatus.getLocationX(),marsRover.getLocationX());
+
+    }
+
 }
