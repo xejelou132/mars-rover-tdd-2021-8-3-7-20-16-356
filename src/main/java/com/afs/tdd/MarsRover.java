9dd6590efea6ac3572d0925c5c1d180257d0ba06
Darlen Kitz Suizo <suizoda@oocl.com>
2021-08-05 21:42:03 +0800

    [MarsRover][Kitz]Feat: Added the requirements for last case

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -24,14 +24,35 @@ public class MarsRover implements RoverMovement {
     }
 
     public void executeCommand(String command) {
-        if (command.equals("M")) {
-            move();
+        if (command.length() > 1) {
+            for (int i = 0; i < command.length(); i++) {
+                getMovements(String.valueOf(command.charAt(i)));
+                getLeft(String.valueOf(command.charAt(i)));
+                getRight(String.valueOf(command.charAt(i)));
+            }
+
+        } else {
+            getMovements(command);
+            getLeft(command);
+            getRight(command);
         }
+    }
+
+    private void getRight(String command) {
+        if (command.equals("R")) {
+            turnRight();
+        }
+    }
+
+    private void getLeft(String command) {
         if (command.equals("L")) {
             turnLeft();
         }
-        if (command.equals("R")) {
-            turnRight();
+    }
+
+    private void getMovements(String command) {
+        if (command.equals("M")) {
+            move();
         }
     }
 
